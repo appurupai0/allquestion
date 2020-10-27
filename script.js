7a282bd36cbb76ab0e9e18263744111fe39ef779
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2020-10-27 20:57:32 +0900

    自分で解いた回答と模範解答のやり方が違った。　もう一回自分で解き直してみよう

--- script.js
+++ script.js
@@ -18,40 +18,71 @@
 
 
 // Q 6
-var clickMe = document.querySelector('#q6-text');
-clickMe.addEventListener('click',function(){
-	this.style.color = 'blue';
-})
+ var clickMe = document.querySelector('#q6-text');
+ clickMe.addEventListener('click',function(){
+ 	this.style.color = 'blue';
+ })
+
+// function changeColor(){
+// 	document.querySelector('#q6-text').style.color = 'green';
+// }
+
 // Q 7
-var q7_text = document.getElementById('q7-text');
-q7_text.addEventListener('mouseover',function(){
-	this.style.backgroundColor = 'pink',
+document.getElementById('q7-text').addEventListener('mouseover',function(){
+	console.log('q7');
+	this.style.backgroundColor = 'pink';
 	this.style.color = 'orange';
 })
 // Q 8
 var q8_text = document.querySelector('#q8-text');
 q8_text.addEventListener('mouseover',function(){
-	this.style.backgroundColor = 'navy',
+	console.log('q8');
+	this.style.backgroundColor = 'navy';
 	this.style.color = 'yellow';
 })
 q8_text.addEventListener('mouseleave',function(){
+	console.log('q8');
 	this.style.backgroundColor = '';
 	this.style.color = '';
 })
 
 // Q 9
-function check(){
-	var input_message = document.querySelector('#q9-input').value;
-	document.getElementById('q9-result').innerHTML = input_message;
-}
+// 自分でやった回答
+// function check(){
+// 	var input_message = document.querySelector('#q9-input').value;
+// 	document.getElementById('q9-result').innerHTML = input_message;
+// }
+
+// 模範解答--------------------------------------------------------
+// ボタンが押された時のイベントを追加するための変数
+let q9_btn = document.querySelector('#q9-btn');
+q9_btn.addEventListener('click',function(){
+	// 入力された文字を取得するための変数
+	var q9_input = document.querySelector('#q9-input');
+
+	// 文字を表示するための変数
+	var q9_result = document.querySelector('#q9-result');
+	q9_result.textContent = q9_input.value;
+})
+// ----------------------------------------------------------------
+
 
 // Q 10
 function button(){
+
+	// 60歳以上の場合、「60歳以上のの方は、半額です」と表示してください
 	var old = document.querySelector('#q10-input').value;
-	if(old >= 20){
+	if(old >= 60){
+		var old60 = '60歳以上の方は半額です。'
+		document.getElementById('q10-result').innerHTML = old60;
+	}else if(old >= 20){
 		document.getElementById('q10-result').innerHTML = old;
 	}else{
-		var oldNot = '20歳未満の方の利用は禁止です。'
+		var oldNot = '20歳未満の方の利用は禁止です'
 		document.getElementById('q10-result').innerHTML = oldNot;
 	}
 }
+
+//　Q 10の問題や　Q9の問題を模範解答のやり方でもう一回やってみる、自分でやってたのは模範解答とは違うやり方
+
+
